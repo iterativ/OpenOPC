462b0270c70bcf5464f7b46323dc321257c2c853
Lorenz Padberg <lorenz.padberg@gmail.com>
2016-02-29 10:33:01 +0100

    Added Semantic versioning 1.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="OpenOPC",
-  version='1.2',
+  version='1.2.0',
   download_url='https://github.com/iterativ/OpenOPC/tarball/master',
   description="This is a clone of http://openopc.sourceforge.net modified to be used with distutils",
   keywords='python, opc, openopc',
