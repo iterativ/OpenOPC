c99f89124c6bf988968e65016a821129c8d25e4c
Lorenz Padberg <lorenz.padberg@gmail.com>
2016-02-29 10:28:15 +0100

    made version a string changed download url

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
-#~ import os, sys, glob, fnmatch
 
 setup(name="OpenOPC",
-  version=1.2,
-  download_url='https://github.com/ingenuitas/OpenOPC/zipball/master',
+  version='1.2',
+  download_url='https://github.com/iterativ/OpenOPC/tarball/master',
   description="This is a clone of http://openopc.sourceforge.net modified to be used with distutils",
   keywords='python, opc, openopc',
   url='http://openopc.sourceforge.net',
