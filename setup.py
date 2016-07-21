4358669d2a539fcf6982d316e277db351eb85b60
Lorenz Padberg <lorenz.padberg@gmail.com>
2016-07-21 18:29:13 +0200

    installation check

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 from OpenOPC import __version__ as version
 
 setup(name="OpenOPC",
-  version=version,
-  download_url='https://github.com/iterativ/OpenOPC/tarball/master',
-  description="This is a clone of http://openopc.sourceforge.net modified to be used with distutils",
-  keywords='python, opc, openopc',
-  url='http://openopc.sourceforge.net',
-  license='GPLv2',
-  packages = find_packages(exclude=['ez_setup']),
-  zip_safe = False,
-  )
-
+      version=version,
+      download_url='https://github.com/iterativ/OpenOPC/tarball/master',
+      description="This is a clone of http://openopc.sourceforge.net modified to be used with distutils",
+      keywords='python, opc, openopc',
+      url='http://openopc.sourceforge.net',
+      license='GPLv2',
+      packages=find_packages(exclude=['ez_setup']),
+      zip_safe=False,
+      )
